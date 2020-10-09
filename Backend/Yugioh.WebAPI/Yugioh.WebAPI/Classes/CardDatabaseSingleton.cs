6b2db7f00cdd6b0152ce5428af7432c7bfc2c1b6
Tafixados <pranas.jarusevicius@gmail.com>
2020-10-09 13:38:08 +0300

    Singleton card database skeleton

---
+++ Backend/Yugioh.WebAPI/Yugioh.WebAPI/Classes/CardDatabaseSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yugioh.WebAPI.Classes
+{
+    public class CardDatabaseSingleton
+    {
+        private static CardDatabaseSingleton _instance = new CardDatabaseSingleton();
+        private static readonly object _padlock = new object();
+        private CardDatabaseSingleton() { }     //private makes it so no new copies of the singleton instance can be created
+
+        public static CardDatabaseSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_padlock)     //locks and instantiates singleton thread
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new CardDatabaseSingleton();
+                        }
+                    }
+
+                }
+
+                return _instance;
+            }
+        }
+
+        public void ReturnCardInfo(int id)
+        {
+
+        }
+
+    }
+}
