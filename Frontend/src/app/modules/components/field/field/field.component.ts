b2bb18fd74eaacc436a7327592340ee3ae309bc8
Skarbalius <48081382+Skarbalius@users.noreply.github.com>
2020-11-27 14:06:16 +0200

    f1

--- Frontend/src/app/modules/components/field/field/field.component.ts
+++ Frontend/src/app/modules/components/field/field/field.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { IField } from '../../../../shared/models/field';
 
-@Component({
+Component({
   selector: 'app-field',
   templateUrl: './field.component.html',
-  styleUrls: ['./field.component.scss']
 })
-export class FieldComponent implements OnInit {
+export class HealthComponent implements OnInit {
 
-  constructor() { }
+  @Input() field: IField;
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
-
 }
